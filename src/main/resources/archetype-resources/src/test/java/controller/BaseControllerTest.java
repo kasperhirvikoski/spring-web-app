931f5b979b3910869d0b532ec9bbdc4e4567d026
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2012-11-17 18:43:26 +0200

    Add basic testing

---
+++ src/main/resources/archetype-resources/src/test/java/controller/BaseControllerTest.java
@@ -0,0 +1,38 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.controller;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring-context.xml",
+                                   "file:src/main/webapp/WEB-INF/spring-front-controller-servlet.xml"})
+public class BaseControllerTest {
+
+    @BeforeClass
+    public static void setUpClass() {}
+
+    @AfterClass
+    public static void tearDownClass() {}
+
+    @Before
+    public void setUp() {}
+
+    @After
+    public void tearDown() {}
+
+    @Test
+    public void youShallPass() {
+
+        Assert.assertTrue(true);
+    }
+}
