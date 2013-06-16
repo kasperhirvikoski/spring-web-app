1f7c6023c97c3022cfcc8a5678fa007888fc3dac
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-06-16 17:58:21 +0300

    Adhere to Checkstyle rules

--- src/main/resources/archetype-resources/src/test/java/controller/BaseControllerTest.java
+++ src/main/resources/archetype-resources/src/test/java/controller/BaseControllerTest.java
@@ -14,8 +14,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring-context.xml",
-                                   "file:src/main/webapp/WEB-INF/spring-front-controller-servlet.xml"})
+@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring-context.xml",
+                                    "file:src/main/webapp/WEB-INF/spring-front-controller-servlet.xml" })
 public class BaseControllerTest {
 
     @BeforeClass
