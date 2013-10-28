fda2d4b743ca64cbe1ab73f1f0344beb4bda1559
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2013-10-28 12:04:31 +0200

    Separate import groups

--- src/main/resources/archetype-resources/src/test/java/controller/BaseControllerTest.java
+++ src/main/resources/archetype-resources/src/test/java/controller/BaseControllerTest.java
@@ -9,8 +9,10 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
