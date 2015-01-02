106aa937942fcd7284414fb487baeb970be64a49
Kasper Hirvikoski <kasper.hirvikoski@gmail.com>
2015-01-02 12:06:31 +0200

    @ContextConfiguration takes an array of paths by default

--- src/main/resources/archetype-resources/src/test/java/controller/BaseControllerTest.java
+++ src/main/resources/archetype-resources/src/test/java/controller/BaseControllerTest.java
@@ -16,8 +16,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring-context.xml",
-                                    "file:src/main/webapp/WEB-INF/spring-front-controller-servlet.xml" })
+@ContextConfiguration({ "file:src/main/webapp/WEB-INF/spring-context.xml",
+                        "file:src/main/webapp/WEB-INF/spring-front-controller-servlet.xml" })
 public class BaseControllerTest {
 
     @BeforeClass
